35be53ae0dba935d7226ea766ab1ffdb13a2ad41
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2021-05-17 15:48:49 +0530

    added specific time to show notification

--- App.js
+++ App.js
@@ -10,7 +10,7 @@
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View, Switch, Platform } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View, Switch, Platform, PushNotificationIOS } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import thunk from 'redux-thunk';
 import { AdMobBanner, PublisherBanner } from 'react-native-admob';
@@ -30,83 +30,87 @@ const middleware = applyMiddleware(thunk);
 // Create store
 let store = createStore(reducers, middleware);
 
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+
+    // process the notification
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    // notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+
+    // process the action
+  },
+
+  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function (err) {
+    console.error(err.message, err);
+  },
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: Platform.OS === 'ios'
+});
+
+PushNotification.createChannel(
+  {
+    channelId: "xxx1", // (required)
+    channelName: "My channel", // (required)
+    channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+    playSound: false, // (optional) default: true
+    soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+    importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+);
+
+var today = new Date();
+let now = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 18, 0, 0);
+
+PushNotification.localNotificationSchedule({
+  //... You can use all the options from localNotifications
+  channelId: "xxx1",
+  message: "Have you checked the prices today?🤔 ", // (required)
+  date: new Date(now), // in 60 secs
+  allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+  repeatType: "day",
+});
+
+
 const App = (props) => {
 
   const [isDark, setIsDark] = useState(false);
   const toggleSwitch = () => setIsDark(previousState => !previousState);
-
-  // Must be outside of any component LifeCycle (such as `componentDidMount`).
-  PushNotification.configure({
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function (token) {
-      console.log("TOKEN:", token);
-    },
-
-    // (required) Called when a remote is received or opened, or local notification is opened
-    onNotification: function (notification) {
-      console.log("NOTIFICATION:", notification);
-
-      // process the notification
-
-      // (required) Called when a remote is received or opened, or local notification is opened
-      notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-
-    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    onAction: function (notification) {
-      console.log("ACTION:", notification.action);
-      console.log("NOTIFICATION:", notification);
-
-      // process the action
-    },
-
-    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    onRegistrationError: function (err) {
-      console.error(err.message, err);
-    },
-
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-      alert: true,
-      badge: true,
-      sound: true,
-    },
-
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-
-    /**
-     * (optional) default: true
-     * - Specified if permissions (ios) and token (android and ios) will requested or not,
-     * - if not, you must call PushNotificationsHandler.requestPermissions() later
-     * - if you are not using remote notification or do not have Firebase installed, use this:
-     *     requestPermissions: Platform.OS === 'ios'
-     */
-    requestPermissions: Platform.OS === 'ios'
-  });
-
-  PushNotification.createChannel(
-    {
-      channelId: "xxx1", // (required)
-      channelName: "My channel", // (required)
-      channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
-      playSound: false, // (optional) default: true
-      soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-      importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-      vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
-    },
-    (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
-  );
-
-  PushNotification.localNotificationSchedule({
-    //... You can use all the options from localNotifications
-    channelId: "xxx1",
-    message: "Have you checked the prices today?💦 ", // (required)
-    date: new Date(Date.now() + 15 * 1000), // in 60 secs
-    allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
-  });
-
 
   const Tab = createBottomTabNavigator();
 
